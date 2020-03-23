8adfe5edd36dde75430a445716f4b95b99cab732
Tahseen Anwar Tahir <37225838+tahseenat@users.noreply.github.com>
2020-03-23 15:19:10 +0530

    Refactor

--- tweets.py
+++ tweets.py
@@ -8,26 +8,26 @@ import tweepy
 from textblob import TextBlob
 import csv
 
-a=[]
+a = []
 
-consumer_key= ''
-consumer_secret= ''
+consumer_key = ''
+consumer_secret = ''
 
-access_token=''
-access_token_secret=''
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth,wait_on_rate_limit=True)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 # Open/Create a file to append data
-csvFile = open('enjoy.csv', 'w',newline='')
-#Use csv Writer
+csvFile = open('enjoy.csv', 'w', newline='')
+# Use csv Writer
 csvWriter = csv.writer(csvFile)
 
-for tweet in tweepy.Cursor(api.search,q="#enjoy",count=200,lang="en",since="2017-04-03").items():
+for tweet in tweepy.Cursor(api.search, q="#enjoy", count=200, lang="en", since="2017-04-03").items():
     analysis = TextBlob(tweet.text)
-    print (tweet.created_at, tweet.text)
-    
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('ascii','ignore')])
+    print(tweet.created_at, tweet.text)
+
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('ascii', 'ignore')])
